fe5821db5b844567175ed3c2810570e71c2dc602
janzenxu <thejanzenxu@outlook.com>
2023-10-03 00:03:19 +0800

    23_10

--- green/main.py
+++ green/main.py
@@ -61,82 +61,6 @@ def find_roi():
     return rect_max
 
 
-def find_screen(rect_x, rect_y, rect_w, rect_h, border):
-    video = cv.VideoCapture(0, cv.CAP_V4L2)
-    video.set(cv.CAP_PROP_BUFFERSIZE, 1)
-
-    ctr_x, ctr_y = rect_w / 2 + border, rect_h / 2 + border
-    pts_pre = [(ctr_x, ctr_y)] * 4
-    frame_count = 0
-    while video.isOpened():
-        ret, frame = video.read()
-        if ret:
-            frame_roi = frame[rect_y - border: rect_y + rect_h + border, rect_x - border: rect_x + rect_w + border, :].copy()
-            ret_screen, screen_pts = vis.find_screen_pts(frame_roi)
-            if ret_screen:
-                screen_pts_sorted = [(ctr_x, ctr_y)] * 4
-                for pt in screen_pts:
-                    if (pt[0] - ctr_x) * (pt[1] - ctr_y) > 0:
-                        if pt[0] - ctr_x > 0:
-                            idx = 0
-                        else:
-                            idx = 2
-                    else:
-                        if pt[0] - ctr_x > 0:
-                            idx = 3
-                        else:
-                            idx = 1
-                    screen_pts_sorted[idx] = pt
-
-                if np.allclose(np.array(screen_pts_sorted), np.array(pts_pre), atol=2):
-                    frame_count += 1
-                else:
-                    frame_count = 0
-
-                pts_pre = (0.7 * np.array(pts_pre) + 0.3 * np.array(screen_pts_sorted)).tolist()
-
-                if frame_count >= 10:
-                    screen_pts_best = pts_pre
-
-                    for pt in screen_pts_best:
-                        pt[0] = int(pt[0] - border + rect_x)
-                        pt[1] = int(pt[1] - border + rect_y)
-
-                    ctr_x_temp = (screen_pts_best[0][0] + screen_pts_best[2][0]) / 2
-                    ctr_y_temp = (screen_pts_best[0][1] + screen_pts_best[2][1]) / 2
-                    ctr_x = (screen_pts_best[1][0] + screen_pts_best[3][0]) / 2
-                    ctr_y = (screen_pts_best[1][1] + screen_pts_best[3][1]) / 2
-                    ctr_x = int((ctr_x + ctr_x_temp) / 2)
-                    ctr_y = int((ctr_y + ctr_y_temp) / 2)
-
-                    video.release()
-                    return screen_pts_best, (ctr_x, ctr_y)
-
-
-def find_box(rect_x, rect_y, rect_w, rect_h, border):
-    global frame_stack
-
-    ctr_x, ctr_y = rect_w / 2 + border, rect_h / 2 + border
-    pts_pre = [(ctr_x, ctr_y)] * 4
-    frame_count = 0
-    while True:
-        frame = frame_stack[-1]
-        ret_box, box_pts = vis.find_box_pts(frame)
-        if ret_box:
-            if np.allclose(np.array(box_pts), np.array(pts_pre), atol=2):
-                frame_count += 1
-            else:
-                frame_count = 0
-            pts_pre = (0.5 * np.array(pts_pre) + 0.5 * np.array(box_pts)).tolist()
-
-            if frame_count >= 10:
-                box_pts_best = pts_pre
-
-                for pt in box_pts_best:
-                    pt[0], pt[1] = vis.coordinate_tansform(pt[0], pt[1], border, rect_x, rect_y)
-                return box_pts_best
-
-
 def video_capture(roi_x, roi_y, roi_w, roi_h, border_width):
     global frame_stack
     video = cv.VideoCapture(0, cv.CAP_V4L2)
@@ -166,71 +90,19 @@ def message_send():
     uart.write(bytes.fromhex("%04x" % obj_y))
 
 
-def laser_control(center, border, roi_x_l, roi_y_l, rect_w, rect_h, trace_pts):
-    global uart, frame_stack
-
-    while True:
-        uart.flushInput()
-        message_count = uart.inWaiting()
-        if message_count != 0:
-            recv = uart.readline()
-            print(recv)
-            if recv == b"2\n":
-                reset_center(center, border, roi_x_l, roi_y_l)
-            elif recv == b"3\n":
-                trace(trace_pts, border, roi_x_l, roi_y_l)
-                reset_center(center, border, roi_x_l, roi_y_l)
-            elif recv == b"4\n":
-                box_points = find_box(roi_x_l, roi_y_l, rect_w, rect_h, border)
-                box_trace = vis.get_trace(box_points, 21)
-                trace(box_trace, border, roi_x_l, roi_y_l)
-                reset_center(center, border, roi_x_l, roi_y_l)
-        time.sleep(0.1)
-
-
-def reset_center(center, border, roi_x_l, roi_y_l):
+def laser_control(border, roi_x_l, roi_y_l):
     global obj_x, obj_y, var_x, var_y, frame_stack
-
-    obj_x = center[0]
-    obj_y = center[1]
 
     while True:
         frame = frame_stack[-1]
+        ret_red, obj_x_roi, obj_y_roi = vis.find_red_pt(frame)
+        ret_green, var_x_roi, var_y_roi = vis.find_green_pt(frame)
 
-        ret_red, var_x_roi, var_y_roi = vis.find_red_pt(frame)
-        if ret_red:
+        if ret_green and ret_red:
             var_x, var_y = vis.coordinate_tansform(var_x_roi, var_y_roi, border, roi_x_l, roi_y_l)
-            message_send()
-            if vis.get_distance((obj_x, obj_y), (var_x, var_y)) < 2:
-                break
-
-
-def trace(trace_pts, border, roi_x_l, roi_y_l):
-    global obj_x, obj_y, var_x, var_y, frame_stack
-
-    obj_x = trace_pts[0][0]
-    obj_y = trace_pts[0][1]
-    trace_idx = 0
-
-    while True:
-        frame = frame_stack[-1]
-        t0 = time.time()
-
-        ret_red, var_x_roi, var_y_roi = vis.find_red_pt(frame)
-        if ret_red:
-            var_x, var_y = vis.coordinate_tansform(var_x_roi, var_y_roi, border, roi_x_l, roi_y_l)
-            if vis.get_distance((obj_x, obj_y), (var_x, var_y)) < 15:
-                trace_idx += 1
+            obj_x, obj_y = vis.coordinate_tansform(obj_x_roi, obj_y_roi, border, roi_x_l, roi_y_l)
 
             message_send()
-
-            if trace_idx == len(trace_pts):
-                # trace_idx = 0
-                break
-
-            obj_x, obj_y = trace_pts[trace_idx]
-        t1 = time.time()
-        print(t1 - t0)
 
 
 if __name__ == "__main__":
@@ -238,15 +110,10 @@ if __name__ == "__main__":
 
     border_width = 10
 
-    screen_points, screen_center = find_screen(roi_x, roi_y, roi_w, roi_h, border_width)
-
-    screen_step = 10
-    screen_trace = vis.get_trace(screen_points, screen_step)
-
     uart.write(b"A")
 
     thread_video_capture = th.Thread(target=video_capture, args=(roi_x, roi_y, roi_w, roi_h, border_width,))
-    thread_laser_control = th.Thread(target=laser_control, args=(screen_center, border_width, roi_x, roi_y, roi_w, roi_h, screen_trace,))
+    thread_laser_control = th.Thread(target=laser_control, args=(border_width, roi_x, roi_y, ))
 
     thread_video_capture.start()
     thread_laser_control.start()
--- green/vision.py
+++ green/vision.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+
 def morph_open(img_in, ksize):
     if ksize == 0:
         img_out = img_in
@@ -36,6 +37,7 @@ def morph_dilate(img_in, ksize):
         img_out = cv.dilate(img_in, kernel)
     return img_out
 
+
 def morph(img_in, erode_ksize, dilate_ksize, open_ksize, close_ksize):
     img_erode = morph_erode(img_in, erode_ksize)
     img_dilate = morph_dilate(img_erode, dilate_ksize)
@@ -43,18 +45,19 @@ def morph(img_in, erode_ksize, dilate_ksize, open_ksize, close_ksize):
     img_out = morph_close(img_open, close_ksize)
     return img_out
 
+
 def find_red_pt(img_in):
     blur_ksize = 1
     img_blur = cv.GaussianBlur(img_in, (blur_ksize, blur_ksize), 0)
-    
+
     img_hsv = cv.cvtColor(img_blur, cv.COLOR_BGR2HSV)
     lower_range_red = np.array([140, 0, 220])
     upper_range_red = np.array([255, 70, 255])
-    
+
     img_binary = cv.inRange(img_hsv, lower_range_red, upper_range_red)
     img_morph = morph(img_binary, 1, 5, 1, 3)
     cnts, hier = cv.findContours(img_morph, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
-    
+
     obj_x = 0
     obj_y = 0
     if cnts:
@@ -65,32 +68,60 @@ def find_red_pt(img_in):
         obj_y = int(moment_cnt['m01'] / moment_cnt['m00'])
     else:
         ret = False
-    
+
     return ret, obj_x, obj_y
+
+
+def find_green_pt(img_in):
+    blur_ksize = 1
+    img_blur = cv.GaussianBlur(img_in, (blur_ksize, blur_ksize), 0)
+
+    img_hsv = cv.cvtColor(img_blur, cv.COLOR_BGR2HSV)
+    lower_range_green = np.array([35, 43, 46])
+    upper_range_green = np.array([80, 255, 255])
+
+    img_binary = cv.inRange(img_hsv, lower_range_green, upper_range_green)
+    img_morph = morph(img_binary, 1, 5, 1, 3)
+    cnts, hier = cv.findContours(img_morph, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
+    obj_x = 0
+    obj_y = 0
+    if cnts:
+        ret = True
+        cnt = cnts[0]
+        moment_cnt = cv.moments(cnt)
+        obj_x = int(moment_cnt['m10'] / moment_cnt['m00'])
+        obj_y = int(moment_cnt['m01'] / moment_cnt['m00'])
+    else:
+        ret = False
+
+    return ret, obj_x, obj_y
+
 
 def find_screen_pts(img_in):
     img_gray = cv.cvtColor(img_in, cv.COLOR_BGR2GRAY)
-    
+
     clahe = cv.createCLAHE(3, (7, 7))
     img_equalize = clahe.apply(img_gray)
-    
-    ctr_y, ctr_x = int(img_gray.shape[0]/2), int(img_gray.shape[1]/2)
-    
+
+    ctr_y, ctr_x = int(img_gray.shape[0] / 2), int(img_gray.shape[1] / 2)
+
     corners = cv.goodFeaturesToTrack(img_equalize, 8, 0.03, 80)
     corner_pts = []
-    
+
     if len(corners) < 4:
         ret = False
     else:
         ret = True
         for corner in corners:
             corner_x, corner_y = corner.ravel()
-            distance = np.sqrt((corner_x-ctr_x)**2 + (corner_y-ctr_y)**2)
+            distance = np.sqrt((corner_x - ctr_x)**2 + (corner_y - ctr_y)**2)
             corner_pts.append((int(corner_x), int(corner_y)))
-        
-        corner_pts = sorted(corner_pts, key=lambda p:np.sqrt((p[0]-ctr_x)**2 + (p[1]-ctr_y)**2), reverse=True)
+
+        corner_pts = sorted(corner_pts, key=lambda p: np.sqrt((p[0] - ctr_x)**2 + (p[1] - ctr_y)**2), reverse=True)
         corner_pts = corner_pts[:4]
     return ret, corner_pts
+
 
 def find_box_pts(img_in):
     blur_ksize = 3
@@ -98,23 +129,23 @@ def find_box_pts(img_in):
     img_gray = cv.cvtColor(img_blur, cv.COLOR_BGR2GRAY)
     ret, img_binary = cv.threshold(img_gray, 100, 255, cv.THRESH_BINARY_INV)
     img_morph = morph(img_binary, 3, 1, 1, 2)
-    
+
     min_area = 9000
     max_area = 15000
-    
+
     box_pts = []
-    
+
     ret = False
     cnts, hier = cv.findContours(img_morph, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
     if cnts:
         for idx, cnt in enumerate(cnts):
             current_hierarchy = hier[0][idx]
             next_idx, prev_idx, child_idx, parent_idx = current_hierarchy
-                    
+
             if min_area < cv.contourArea(cnt) < max_area and parent_idx != -1:
-                ret = True 
+                ret = True
                 inner_cnt = cnt
-            
+
         if ret:
             rect = cv.minAreaRect(inner_cnt)
             coordinate = np.int0(cv.boxPoints(rect))
@@ -123,22 +154,24 @@ def find_box_pts(img_in):
             # cv.polylines(img_in, [coordinate], )
     return ret, box_pts
 
+
 def get_distance(p1, p2):
-    distance = np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
+    distance = np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
     return distance
+
 
 def get_trace(pts, step):
     trace_pts = []
     for i in range(len(pts)):
-        x_step = (np.array(pts[i+1 if i+1<4 else 0]) - np.array(pts[i]))[0] / step
-        y_step = (np.array(pts[i+1 if i+1<4 else 0]) - np.array(pts[i]))[1] / step
+        x_step = (np.array(pts[i + 1 if i + 1 < 4 else 0]) - np.array(pts[i]))[0] / step
+        y_step = (np.array(pts[i + 1 if i + 1 < 4 else 0]) - np.array(pts[i]))[1] / step
         for j in range(step):
-            trace_pts.append((int(pts[i][0] + x_step*j), int(pts[i][1] + y_step*j)))
+            trace_pts.append((int(pts[i][0] + x_step * j), int(pts[i][1] + y_step * j)))
     trace_pts.append(tuple(pts[0]))
     return trace_pts
+
 
 def coordinate_tansform(coor_x, coor_y, border, roi_x_l, roi_y_l):
     real_x = int(coor_x - border + roi_x_l)
     real_y = int(coor_y - border + roi_y_l)
     return real_x, real_y
-
